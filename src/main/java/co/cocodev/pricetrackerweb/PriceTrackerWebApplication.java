544780db410bc214ae8d17061d3392417e17ad9d
Diego <dfreyes00@gmail.com>
2022-12-17 21:33:32 -0500

    first commit

---
+++ src/main/java/co/cocodev/pricetrackerweb/PriceTrackerWebApplication.java
@@ -0,0 +1,13 @@
+package co.cocodev.pricetrackerweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceTrackerWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PriceTrackerWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/cocodev/pricetrackerweb/PriceTrackerWebApplicationTests.java
@@ -0,0 +1,13 @@
+package co.cocodev.pricetrackerweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceTrackerWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
